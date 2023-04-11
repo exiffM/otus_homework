9f909b287b9b8119272fd634c43427ad09775276
exiffM <tommy_wersetty@mail.ru>
2023-04-11 22:59:35 +0300

    HW2 is completed!

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,10 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
@@ -34,12 +34,39 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qwe\nfd` /*new test*/}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+/*Rus Test*/
+func TestUnpackRus(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "р4ус2е5н", expected: "рррруссееееен"},
+		{input: "ларкке", expected: "ларкке"},
+		{input: "", expected: ""},
+		{input: "вал0л", expected: "вал"},
+		// uncomment if task with asterisk completed
+		{input: `йцу\4\5`, expected: `йцу45`},
+		{input: `йцу\45`, expected: `йцу44444`},
+		{input: `йцу\\5`, expected: `йцу\\\\\`},
+		{input: `йцу\\\3`, expected: `йцу\3`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
