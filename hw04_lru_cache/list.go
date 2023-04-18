5ebb2603018a4035fe4df2bd23d3398dc22a768b
exiffM <tommy_wersetty@mail.ru>
2023-04-18 23:08:30 +0300

    Test commit hw4

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,50 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	Head *ListItem
+	Tail *ListItem
+	Size int
+}
+
+func (lst *list) Len() int {
+	return lst.Size
+}
+
+func (lst *list) Front() *ListItem {
+	return lst.Head
+}
+
+func (lst *list) Back() *ListItem {
+	return lst.Tail
+}
+
+func (lst *list) PushFront(v interface{}) *ListItem {
+	var newHead = new(ListItem)
+	newHead.Next = lst.Head
+	newHead.Prev = nil
+	lst.Head.Prev = newHead
+	lst.Head = newHead
+	lst.Size++
+	return lst.Head
+}
+
+func (lst *list) PushBack(v interface{}) *ListItem {
+	var newTail = new(ListItem)
+	newTail.Next = nil
+	newTail.Prev = lst.Tail
+	lst.Tail.Next = newTail
+	lst.Tail = newTail
+	lst.Size++
+	return lst.Tail
+}
+
+func (lst *list) Remove(i *ListItem) {
+
+	lst.Size--
+}
+
+func (lst *list) MoveToFront(i *ListItem) {
+
 }
 
 func NewList() List {
