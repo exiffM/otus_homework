dc5089383cf575397221495ffd32731d3ae69fcb
exiffM <tommy_wersetty@mail.ru>
2023-06-12 18:19:18 +0300

    Draft PR

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,5 +10,48 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
-	return nil
+
+	// Первый вариант
+	// out := make(Bi, len(stages))
+	// go func() {
+	// 	defer close(out)
+	// 	for _, stage := range stages {
+	// 		out <- stage(in)
+	// 	}
+	// }()
+
+	// Второй вариант
+	// out := make(Bi, len(stages))
+	// stageIn := make(Bi)
+	// go func() {
+	// 	defer close(stageIn)
+	// 	for data := range in {
+	// 		stageIn <- data
+	// 	}
+	// }()
+
+	// go func() {
+	// 	defer close(out)
+	// 	for _, stage := range stages {
+	// 		out <- stage(stageIn)
+	// 	}
+	// }()
+
+	// Третий вариант
+	// out := make(Bi, len(stages))
+	// stageIn := make(Bi)
+	// go func() {
+	// 	defer close(stageIn)
+	// 	for data := range in {
+	// 		stageIn <- data
+	// 	}
+	// }()
+
+	//
+	// 	defer close(out)
+	// 	for _, stage := range stages {
+	// 		out <- stage(stageIn)
+	// 	}
+
+	return out
 }
