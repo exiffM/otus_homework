a3e30519e446fafab13cb9f6f685612b5390aa5b
exiffM <tommy_wersetty@mail.ru>
2023-06-20 23:31:41 +0300

    Draft pr

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,65 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
+	ErrEmptyInputFile        = errors.New("empty file")
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrPermissionDenied      = errors.New("permission denied")
+	ErrLimitIsInvalid        = errors.New("invalid limit value")
+	ErrOffsetIsInvalid       = errors.New("invalid offset value")
+	ErrEmptyOutputFilePath   = errors.New("empty output file path")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+// limit = 0 - whole file, limit > file size is ok
+// offset <= fileSize
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	info, err := os.Stat(fromPath)
+	// check if file path is empty
+	switch {
+	case err != nil || info.IsDir():
+		return ErrUnsupportedFile
+	case info.Size() == 0:
+		return ErrEmptyInputFile
+	case toPath == "":
+		return ErrEmptyOutputFilePath
+	case info.Mode() < 0o644:
+		return ErrPermissionDenied
+	case limit < 0:
+		return ErrLimitIsInvalid
+	case offset < 0:
+		return ErrOffsetIsInvalid
+	case offset > info.Size():
+		return ErrOffsetExceedsFileSize
+	default:
+		inputFile, err := os.Open(fromPath)
+		defer inputFile.Close()
+		if err != nil {
+			return err
+		}
+
+		outputFile, err := os.Create(toPath)
+		defer outputFile.Close()
+		if err != nil {
+			return err
+		}
+
+		inputFile.Seek(offset, io.SeekStart)
+
+		if limit == 0 || limit > info.Size() {
+			_, err := io.Copy(outputFile, inputFile)
+			return err
+		} else {
+			_, err := io.CopyN(outputFile, inputFile, limit)
+			if err == io.EOF {
+				return nil
+			} else {
+				return err
+			}
+		}
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,96 @@
 package main
 
-import "testing"
+import (
+	"crypto/sha256"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	t.Run("Unsupported file", func(t *testing.T) {
+		err := Copy("testdata", "out.txt", 0, 0)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error is %v", err)
+	})
+
+	t.Run("Empty input file", func(t *testing.T) {
+		err := Copy("/dev/random", "out.txt", 0, 0)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrEmptyInputFile), "actual error is %v", err)
+	})
+
+	t.Run("Empty output file path", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "", 0, 0)
+		require.Truef(t, errors.Is(err, ErrEmptyOutputFilePath), "actual error is %v", err)
+	})
+
+	t.Run("No permission file", func(t *testing.T) {
+		err := Copy("testdata/forbiden.txt", "out.txt", 0, 0)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrPermissionDenied), "actual error is %v", err)
+	})
+
+	t.Run("Invalid limit", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 0, -1)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrLimitIsInvalid), "actual error is %v", err)
+	})
+
+	t.Run("Invalid offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", -10, 1)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrOffsetIsInvalid), "actual error is %v", err)
+	})
+
+	t.Run("Offset exceeds file size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 6618, 1)
+		defer os.Remove("out.txt")
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error is %v", err)
+	})
+
+	t.Run("Successful work, full file", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 0, 0)
+		require.Truef(t, errors.Is(err, nil), "actual error is %v", err)
+		f1, _ := os.Open("testdata/input.txt")
+		defer f1.Close()
+		f2, _ := os.Open("out.txt")
+		defer f2.Close()
+		defer os.Remove(f2.Name())
+
+		h1 := sha256.New()
+		h2 := sha256.New()
+		if _, err := io.Copy(h1, f1); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := io.Copy(h2, f2); err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, h1, h2, "Files' contents are different")
+	})
+
+	t.Run("Successful work, cropped file", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "out.txt", 6000, 1000)
+		require.Truef(t, errors.Is(err, nil), "actual error is %v", err)
+		f1, _ := os.Open("testdata/out_offset6000_limit1000.txt")
+		defer f1.Close()
+		f2, _ := os.Open("out.txt")
+		defer f2.Close()
+		defer os.Remove(f2.Name())
+
+		h1 := sha256.New()
+		h2 := sha256.New()
+		if _, err := io.Copy(h1, f1); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := io.Copy(h2, f2); err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, h1, h2, "Files' contents are different")
+	})
 }
